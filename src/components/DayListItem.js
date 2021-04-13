c124ce0977c4f22952959f81662a6ebc132055e6
Caden <cadenc.7@gmail.com>
2021-04-12 22:48:17 -0600

    first day commit, created sidebar with days

---
+++ src/components/DayListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from 'classnames';
+
+export default function DayListItem(props) {
+
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'text--light': props.spots,
+    'day-list__item--full': props.spots === 0,
+    'text--regular': props.name 
+  });
+
+  const formatSpots = spots => {
+    if (spots === 1) {
+      return `${spots} spot remaining`;
+    } else {
+      return (spots === 0 ? 'no spots remaining' : `${spots} spots remaining`);
+    }
+  };
+
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <div className={dayClass}>
+        <h2 className="text--regular">{props.name}</h2> 
+        <h3 className="text--light">{formatSpots(props.spots)}</h3>
+      </div>
+    </li>
+  );
+}
